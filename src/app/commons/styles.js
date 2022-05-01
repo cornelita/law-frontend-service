d66912152ec3f17c5eabe843c8451b8901f0f48d
cornelita <cornelitalugitasantoso@gmail.com>
2022-05-01 22:28:33 +0700

    feat: add common styles

---
+++ src/app/commons/styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native-web';
+
+const styles = StyleSheet.create({
+  h1: {
+    fontSize: 36,
+    fontWeight: 700,
+    fontFamily: 'Nunito, sans-serif',
+  },
+  h2: {
+    fontSize: 24,
+    fontWeight: 700,
+    fontFamily: 'Nunito, sans-serif',
+  },
+  h3: {
+    fontSize: 18,
+    fontWeight: 700,
+    fontFamily: 'Nunito, sans-serif',
+  },
+  body1: {
+    fontSize: 16,
+    fontFamily: 'Open Sans, sans-serif',
+  },
+});
+
+export default styles;
