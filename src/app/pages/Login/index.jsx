f330e0c6b2bbf68d812f21dba0978ceca493b008
cornelita <cornelitalugitasantoso@gmail.com>
2022-05-03 12:38:08 +0700

    feat: add Login page

--- src/app/pages/Login/index.jsx
+++ src/app/pages/Login/index.jsx
@@ -1,12 +1,90 @@
 import React from 'react';
-import { Text } from 'react-native-web';
+import {
+  Avatar,
+  Button,
+  TextField,
+  Grid,
+  Box,
+  Typography,
+  Container
+} from '@mui/material';
+import { LockOutlined } from '@mui/icons-material';
+import { View, Text } from 'react-native-web';
+import { NavLink } from 'react-router-dom';
+
+import styles from 'app/pages/styles';
 
 function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
   return (
-    <Text>
-      Login
-    </Text>
-  )
+    <View style={styles.container}>
+      <Text style={styles.pageTitle}>
+        Welcome to Kami Random!
+      </Text>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlined />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login to Kami Random
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Login
+            </Button>
+            <Grid container>
+              <Grid item>
+                <NavLink to="/signup">
+                  Don't have an account? Sign Up
+                </NavLink>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </View>
+  );
 }
 
 export default Login;
