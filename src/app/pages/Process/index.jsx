a12f394fc4faa1621ff955fd5df0a12ba6f3994f
cornelita <cornelitalugitasantoso@gmail.com>
2022-05-03 12:40:24 +0700

    feat: implement Process page

--- src/app/pages/Process/index.jsx
+++ src/app/pages/Process/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Text } from 'react-native-web';
+import { View, Text } from 'react-native-web';
+import { Grid } from '@mui/material';
+
+import styles from 'app/pages/styles';
+import ProcessStatus from 'app/components/ProcessStatus';
+import { getAllProcess } from './utils';
 
 function Process() {
+  const allProcess = getAllProcess();
+
   return (
-    <Text>
-      Process
-    </Text>
+    <View>
+      <Text style={styles.pageTitle}>
+        Background Process
+      </Text>
+      <Grid container columnSpacing={8} rowSpacing={2.5}>
+        {allProcess.map(({id, data, progress}) => (
+          <Grid item xs={6} key={id}>
+            <ProcessStatus idVideo={data} progress={progress} />
+          </Grid>
+        ))}
+      </Grid>
+    </View>
   )
 }
 
