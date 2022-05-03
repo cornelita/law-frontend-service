b6dcb21fad86f7f3b94b4984ab671d452bd798e1
cornelita <cornelitalugitasantoso@gmail.com>
2022-05-03 12:38:26 +0700

    feat: add SignUp page

--- src/app/pages/SignUp/index.jsx
+++ src/app/pages/SignUp/index.jsx
@@ -1,12 +1,114 @@
 import React from 'react';
-import { Text } from 'react-native-web';
+import {
+  Avatar,
+  Button,
+  TextField,
+  Grid,
+  Box,
+  Typography,
+  Container
+} from '@mui/material';
+import { LockOutlined } from '@mui/icons-material';
+import { View, Text } from 'react-native-web';
+import { NavLink } from 'react-router-dom';
+
+import styles from 'app/pages/styles';
 
 function SignUp() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
   return (
-    <Text>
-      SignUp
-    </Text>
-  )
+    <View style={styles.container}>
+      <Text style={styles.pageTitle}>
+        Welcome to Kami Random!
+      </Text>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlined />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Let's create an account!
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  autoComplete="username"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container>
+              <Grid item>
+                <NavLink to="/login">
+                  Already have an account? Login
+                </NavLink>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </View>
+  );
 }
 
 export default SignUp;
