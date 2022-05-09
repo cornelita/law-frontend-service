2ab0d50eb5a65479004bc5dbee8334283264dddb
cornelita <cornelitalugitasantoso@gmail.com>
2022-05-09 19:47:20 +0700

    feat: api call and reducer

---
+++ src/app/reducers/auth/api.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+
+import config from 'app/config';
+import { login, logout } from './index';
+
+export const loginUser = async (userData, dispatch) => {
+  axios.defaults.headers.common.Authorization = null;
+  try {
+    const response = await axios.post(
+      `${config.API_AUTH_URL}/api/login/`,
+      userData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      },
+    );
+
+    if (response.status !== 200) {
+      const message = `Error: ${
+        response.data?.non_field_errors ||
+        response.data?.error ||
+        'Please make sure the data is correct and try again later'
+      }`;
+      return new Error(message);
+    }
+
+    dispatch(login(response.data.user_info));
+    return response.data;
+  } catch (err) {
+    const message = `Error: ${
+      err.response?.data?.non_field_errors ||
+      err.response?.data?.error ||
+      'Please make sure the data is correct and try again later'
+    }`;
+    return new Error(message);
+  }
+};
+
+export const logoutUser = async (dispatch) => {
+  try {
+    const response = await axios.post(`${config.API_AUTH_URL}/api/logout/`);
+
+    if (response.status !== 204) {
+      const message = `Error: ${
+        response.data?.detail || 'Please try again later'
+      }`;
+      return new Error(message);
+    }
+
+    dispatch(logout());
+  } catch (err) {
+    const message = `Error: ${
+      err.response?.data?.detail || 'Please try again later'
+    }`;
+    return new Error(message);
+  }
+};
---
+++ src/app/reducers/auth/index.js
@@ -0,0 +1,43 @@
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const initialAuthState = {
+  email: '',
+  name: '',
+  username: '',
+  token: '',
+  expiry: '',
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    value: initialAuthState,
+  },
+  reducers: {
+    login: (state, action) => {
+      state.value = {
+        ...state.value,
+        ...action.payload,
+      };
+      axios.defaults.headers.common.Authorization = `Token ${state.value.token}`;
+    },
+    logout: (state) => {
+      state.value = initialAuthState;
+      axios.defaults.headers.common.Authorization = null;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+
+// Timer to handle expired token (source: https://stackoverflow.com/a/71220230)
+export const runLogoutTimer = (dispatch, expiry) => {
+  const timer = Date.parse(expiry) - Date.now() || 9 * 60 * 60 * 1000; // Default: 9 hours
+
+  setTimeout(() => {
+    dispatch(logout());
+  }, timer);
+};
+
+export default authSlice.reducer;
