c1413dd3e5b1e286eb3eb3450af9de6510179e56
cornelita <cornelitalugitasantoso@gmail.com>
2022-05-09 19:25:28 +0700

    feat: create store

---
+++ src/app/store/index.js
@@ -0,0 +1,37 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import authReducer from '../reducers/auth';
+
+const reducers = combineReducers({
+  auth: authReducer,
+});
+
+const persistConfig = {
+  key: 'kamirandom',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export default store;
