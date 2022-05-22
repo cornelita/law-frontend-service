2bed077fb141b7adc108cc4a321a5f89f60a0500
cornelita <cornelitalugitasantoso@gmail.com>
2022-05-23 00:25:04 +0700

    feat: api bulk download service

---
+++ src/app/api/bulkDownload.js
@@ -0,0 +1,46 @@
+import config from 'app/config';
+import axios from 'axios';
+
+const FileDownload = require('js-file-download');
+
+export const bulkDownload = async (videoIds) => {
+  try {
+    const response = await axios.post(
+      `${config.API_BULK_DOWNLOAD_URL}/bulk-download/`,
+      {
+        videoIds,
+      },
+    );
+    return response.data;
+  } catch (err) {
+    const message = `Error: ${
+      err.response?.data?.detail ||
+      'Bulk Download failed, please try again later!'
+    }`;
+    return new Error(message);
+  }
+};
+
+export const getBulkDownload = async (bulkDownloadId) => {
+  try {
+    const response = await axios.get(
+      `${config.API_BULK_DOWNLOAD_URL}/bulk-download/`,
+      {
+        params: {
+          bulkDownloadId,
+        },
+      },
+      {
+        responseType: 'blob',
+      },
+    );
+    FileDownload(response.data, `video-${Date.now()}.zip`);
+    return response;
+  } catch (err) {
+    const message = `Error: ${
+      err.response?.data?.detail ||
+      'Get Bulk Download failed, please try again later!'
+    }`;
+    return new Error(message);
+  }
+};
