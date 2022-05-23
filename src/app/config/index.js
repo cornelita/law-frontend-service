b611864c72f332e3fbc9a265aa085e210c22d1d2
cornelita <cornelitalugitasantoso@gmail.com>
2022-05-23 23:18:45 +0700

    fix: change endpoint bulk download

--- src/app/config/index.js
+++ src/app/config/index.js
@@ -1,6 +1,6 @@
 const config = {
   API_AUTH_URL: 'https://auth-law.herokuapp.com',
-  API_BULK_DOWNLOAD_URL: 'http://localhost:8000',
+  API_BULK_DOWNLOAD_URL: 'http://34.122.32.75:8000',
   API_PLAYLIST_URL: '',
   API_DOWNLOAD_URL: '',
 };
