faea7c9c40f6bf601108841ee6b2650f8c095fee
cornelita <cornelitalugitasantoso@gmail.com>
2022-05-23 14:19:37 +0700

    feat: api base url

--- src/app/config/index.js
+++ src/app/config/index.js
@@ -1,5 +1,8 @@
 const config = {
   API_AUTH_URL: 'https://auth-law.herokuapp.com',
+  API_BULK_DOWNLOAD_URL: 'http://localhost:8000',
+  API_PLAYLIST_URL: '',
+  API_DOWNLOAD_URL: '',
 };
 
 export default config;
