47f1e06ec39df7416f486e826fffbdcac346bc8c
cornelita <cornelitalugitasantoso@gmail.com>
2022-05-27 13:31:05 +0700

    fix: change auth's url

--- src/app/config/index.js
+++ src/app/config/index.js
@@ -1,5 +1,5 @@
 const config = {
-  API_AUTH_URL: 'https://auth-law.herokuapp.com',
+  API_AUTH_URL: 'http://34.168.241.7',
   API_BULK_DOWNLOAD_URL: 'http://34.122.32.75:8000',
   WS_BULK_DOWNLOAD_URL: 'ws://34.122.32.75:8000',
   API_PLAYLIST_URL: 'http://localhost:8002',
