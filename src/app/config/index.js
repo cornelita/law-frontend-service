4efd500008c183a8681294e83137d860400a5e53
cornelita <cornelitalugitasantoso@gmail.com>
2022-05-27 21:14:51 +0700

    fix: change playlist's url

--- src/app/config/index.js
+++ src/app/config/index.js
@@ -2,7 +2,7 @@ const config = {
   API_AUTH_URL: 'http://34.168.241.7',
   API_BULK_DOWNLOAD_URL: 'http://34.122.32.75:8000',
   WS_BULK_DOWNLOAD_URL: 'ws://34.122.32.75:8000',
-  API_PLAYLIST_URL: 'http://localhost:8002',
+  API_PLAYLIST_URL: 'http://35.188.105.71',
   API_DOWNLOAD_URL: 'http://localhost:8003',
 };
 
